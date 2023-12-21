dccdaac4b20dbbbebb16edc18bcbaef6d52cc5f2
22jz0141 <2jz@R841PC21.jz.jec.ac.jp>
2023-12-21 11:04:35 +0900

    商品詳細-1221-1104

--- src/servlet/Product_detail.java
+++ src/servlet/Product_detail.java
@@ -38,7 +38,6 @@ public class Product_detail extends HttpServlet {
 		request.setAttribute("item", item);
 		
 		
-		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/user/product_detail.jsp");
 		dispatcher.forward(request, response);
 	}
