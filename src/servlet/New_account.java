a20e9d39bc9a0d44ca6895d053dd2b0e29345435
22jz0119 <2jz@R841PC18.jz.jec.ac.jp>
2023-12-21 10:45:28 +0900

    フォワード-1221-1045

--- src/servlet/New_account.java
+++ src/servlet/New_account.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,12 +16,11 @@ import javax.servlet.http.HttpServletResponse;
 public class New_account extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/user/new_account.jsp");
+		dispatcher.forward(request, response);
+	
 	}
 
 	/**
@@ -27,7 +28,7 @@ public class New_account extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+//		doGet(request, response);
 	}
 
 }
